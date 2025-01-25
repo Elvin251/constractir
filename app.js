28931951fe61c377f6a82fdfaf0279ae688b0652
Elvin251 <emmmdov485@gmail.com>
2025-01-25 17:03:47 +0400

    d

--- app.js
+++ app.js
@@ -14,16 +14,53 @@
 
 
 // task2
-class Person {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
+// class Person {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+//     celebrateBirthday() {
+//         this.age += 1;
+//     }
+// }
+// let person4 = new Person("Ali", 14);
+// console.log(`evvel ad gunu:${this.name} senin ${person4.age} yasin var`);
+// person4.celebrateBirthday();
+// console.log(`sonra ad gunu: ${this.name} senin ${person4.age} yasin var`);
+
+
+
+
+
+
+
+// homework
+// task1
+class Car {
+    constructor(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
     }
-    celebrateBirthday() {
-        this.age += 1;
+
+    displayInfo() {
+        console.log(`Car Information: ${this.year} ${this.make} ${this.model}`);
     }
 }
-let person4 = new Person("Ali", 14);
-console.log(`evvel ad gunu:${this.name} senin ${person4.age} yasin var`);
-person4.celebrateBirthday();
-console.log(`sonra ad gunu: ${this.name} senin ${person4.age} yasin var`);
+let myCar = new Car("Toyota", "Camry", 2024);
+myCar.displayInfo();
+// task2
+class FootBall {
+    constructor(teamName, totalPlayers, trophies) {
+        this.teamName = teamName;
+        this.totalPlayers = totalPlayers;
+        this.trophies = trophies;
+    }
+
+    calcReservePlayer() {
+        return this.totalPlayers - 11;
+    }
+}
+let myTeam = new FootBall("Real Madrid", 25, 15);
+console.log(`Reserve Players: ${myTeam.calcReservePlayer()}`);
+
